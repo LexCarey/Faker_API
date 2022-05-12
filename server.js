e22ce3f928ca6239974e3c9b45b29b22d3594c63
lexCarey <you@example.com>
2022-05-12 14:42:41 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const { faker } = require('@faker-js/faker');
+const app = express();
+const port = 8000;
+
+app.listen(port, ()=>console.log("Listening to port : " + port));
+
+const createUser = () => {
+    const newUser = {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        phoneNumber: faker.phone.phoneNumber(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        id: Math.floor(Math.random() * 1000)
+    }
+    return newUser
+}
+
+const createCompany = () => {
+    const newCompany = {
+        id: Math.floor(Math.random() * 1000),
+        name: faker.company.companyName(),
+        address: {
+            street: faker.address.streetName(),
+            city: faker.address.cityName(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCode(),
+            country: faker.address.country()
+        }
+    }
+    return newCompany
+}
+
+app.get("/api/users/new", (req, res)=> {
+    const user = createUser()
+    res.json(user)
+})
+
+app.get("/api/companies/new", (req, res)=> {
+    const company = createCompany()
+    res.json(company)
+})
+
+app.get("/api/user/company", (req, res)=> {
+    const both = {
+        user: createUser(),
+        company: createCompany()
+    }
+    res.json(both)
+})
